864a0333003090c750a1517c1df62dee45a608e0
aar-atiq <aar.msdeveloper01@gmail.com>
2021-04-16 17:10:27 +0600

    added navbar

--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -1,34 +1,36 @@
 import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light">
-
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link mr-5" href="#">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link mr-5" href="#">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link mr-5" href="#">Dental Services</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link mr-5 " href="#">Reviews</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link mr-5 " href="#">Blogs</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link mr-5 " href="#">Contact Us</a>
-                    </li>
-                </ul>
+        <nav class="navbar navbar-expand-lg navbar-primary bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Exclusions Photography</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item me-5">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item me-5">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item me-5">
+                            <a class="nav-link" href="#">Services</a>
+                        </li>
+                        <li class="nav-item me-5">
+                            <a class="nav-link " href="#">Dashboard</a>
+                        </li>
+                        <li class="nav-item me-5">
+                            <a class="nav-link " href="#">Contact Us</a>
+                        </li>
+                        <li class="nav-item me-5">
+                            <a class="nav-link " href="#">Login</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
