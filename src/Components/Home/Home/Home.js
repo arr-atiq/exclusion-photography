40bb1a188566c89436eda3bd6c88ba1ed41233b2
aar-atiq <aar.msdeveloper01@gmail.com>
2021-04-18 05:43:28 +0600

    added Projects statistics

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BannerSection from '../BannerSection/BannerSection';
 import Footer from '../Footer/Footer';
 import Portfolio from '../Portfolio/Portfolio';
+import ProjectCounter from '../ProjectCounter/ProjectCounter';
 import Services from '../Services/Services';
 import Testimonials from '../Testimonials/Testimonials';
 
@@ -12,6 +13,7 @@ const Home = () => {
             <Portfolio></Portfolio>
             <Services></Services>
             <Testimonials></Testimonials>
+            <ProjectCounter></ProjectCounter>
             <Footer></Footer>
         </div>
     );
---
+++ src/Components/Home/ProjectCounter/ProjectCounter.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faFeatherAlt, faProjectDiagram } from '@fortawesome/free-solid-svg-icons';
+
+
+const ProjectCounter = () => {
+
+    var happy = setInterval(happyCustomer, 2)
+    var projects = setInterval(RunningProject, 2)
+    var clients = setInterval(ourClients, 2)
+    var writers = setInterval(famousWriter, 2)
+    let count1 = 125000;
+    let count2 = 20;
+    let count3 = 20;
+    let count4 = 5;
+
+    function happyCustomer() {
+        count1++
+        document.querySelector("#number1").innerHTML = count1
+        if (count1 === 125663) {
+            clearInterval(happy)
+        }
+    }
+
+    function RunningProject() {
+        count2++
+        document.querySelector("#number2").innerHTML = count2
+        if (count2 === 57) {
+            clearInterval(projects)
+        }
+    }
+
+    function ourClients() {
+        count3++
+        document.querySelector("#number3").innerHTML = count3
+        if (count3 === 120) {
+            clearInterval(clients)
+        }
+    }
+
+    function famousWriter() {
+        count4++
+        document.querySelector("#number4").innerHTML = count4
+        if (count4 === 12) {
+            clearInterval(writers)
+        }
+    }
+
+
+    return (
+        <div className="container">
+            <h2 className="text-center text-primary mt-4 mb-5 pt-5">Projects statistics</h2>
+            <div style={{ margin:"0"}} class="row d-flex justify-content-center">
+                <div className="col-md-3 text-center mt-5 mb-5 mt-5 mt-4">
+                    <i><FontAwesomeIcon icon={faUser} /></i>
+                    <p id="number1" class="number">125,663</p>
+                    <span></span>
+                    <p>Happy Customers</p>
+                </div>
+                <div className="col-md-3 text-center mt-5 mb-5 mt-5 mt-4">
+                    <i><FontAwesomeIcon icon={faProjectDiagram} /></i>
+                    <p id="number2" class="number">57+</p>
+                    <span></span>
+                    <p>Running Project</p>
+                </div>
+                <div className="col-md-3 text-center mt-5 mb-5 mt-5 mt-4">
+                    <i><FontAwesomeIcon icon={faUser} /></i>
+                    <p id="number3" class="number">120</p>
+                    <span></span>
+                    <p>Our Clients</p>
+                </div>
+                <div className="col-md-3 text-center mt-5 mb-5 mt-5 mt-4">
+                    <i><FontAwesomeIcon icon={faFeatherAlt} /></i>
+                    <p id="number4" class="number">12</p>
+                    <span></span>
+                    <p>Famous Writers</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectCounter;
