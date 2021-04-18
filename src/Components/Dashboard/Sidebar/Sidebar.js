7b01f97b5b5feeabf499c70bdd992e01d0d912f3
aar-atiq <aar.msdeveloper01@gmail.com>
2021-04-18 07:57:34 +0600

    added programming hero admin

--- src/Components/Dashboard/Sidebar/Sidebar.js
+++ src/Components/Dashboard/Sidebar/Sidebar.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './Sidebar.css';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog, faSignOutAlt, faCalendar, faHome, faGripHorizontal, faUserPlus, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { faFileAlt } from '@fortawesome/free-regular-svg-icons'
+import { UserContext } from '../../../App';
 
 const Sidebar = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [isDoctor, setIsDoctor] = useState(false);
     return (
         <div className="sidebar d-flex flex-column justify-content-between col-md-2  py-5 px-4">
             <ul className="list-unstyled">
-                <li>
-                    <Link to="/dashboard" >
-                        <FontAwesomeIcon icon={faGripHorizontal} /> <a href="/dashboard">Dashboard</a>
-                    </Link>
-                </li>
+
                 <li>
                     <Link to="/" >
                         <FontAwesomeIcon icon={faHome} /> <a href="/home">Home</a>
                     </Link>
                 </li>
-                <div>
+                {<div>
                     <li>
                         <Link to="/orderList" >
                             <FontAwesomeIcon icon={faCalendar} /> <a href="/order">Order List</a>
@@ -36,26 +35,21 @@ const Sidebar = () => {
                         </Link>
                     </li>
                     <li>
-                        <Link to="/confirmBooking"  >
-                            <FontAwesomeIcon icon={faCog} /> <a href="/confirmBooking">Confirm Booking</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/bookingList"  >
-                            <FontAwesomeIcon icon={faCog} /> <a href="/bookingList">Booking List</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/review"  >
-                            <FontAwesomeIcon icon={faCog} /> <a href="/review">Review</a>
-                        </Link>
-                    </li>
-                    <li>
                         <Link to="/manageServices"  >
                             <FontAwesomeIcon icon={faUserPlus} /> <a href="/manageServices">Manage Services</a>
                         </Link>
                     </li>
-                </div>
+                </div>}
+                <li>
+                    <Link to="/bookingList"  >
+                        <FontAwesomeIcon icon={faCog} /> <a href="/bookingList">Booking List</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/review"  >
+                        <FontAwesomeIcon icon={faCog} /> <a href="/review">Review</a>
+                    </Link>
+                </li>
             </ul>
             <div>
                 <Link to="/" ><FontAwesomeIcon icon={faSignOutAlt} /> <span>Logout</span></Link>
--- src/Components/MakeAdmin/MakeAdmin.js
+++ src/Components/MakeAdmin/MakeAdmin.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from "react-hook-form";
+import { UserContext } from '../../App';
+import Sidebar from '../Dashboard/Sidebar/Sidebar';
 
 const MakeAdmin = () => {
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+
+        const newAdminData = {
+            adminData: data
+        }
+        const url = `http://localhost:8000/makeAdmin`;
+        fetch(url, {
+            method: 'POST',
+            headers: { 'content-Type': 'application/json' },
+            body: JSON.stringify(newAdminData)
+        })
+            .then(res => {
+                alert("Admin added successfully")
+            })
+    }
     return (
-        <div>
-            <h1>this is make admin</h1>
-        </div>
+        <section>
+            <div className="row mt-5">
+                <div className="col-md-3">
+                    <Sidebar></Sidebar>
+                </div>
+                <div className="col-md-8">
+                    <h3 className="text-center mt-5 text-danger">Add Another Admin!</h3>
+                    <form className="admin-form designForm" onSubmit={handleSubmit(onSubmit)}>
+                        <input name="name" className="form-control" placeholder="Your Name" {...register('name')} />
+                        <br />
+                        <input name="email" className="form-control" placeholder="Enter Your Email" {...register('email')} />
+                        <br />
+                        <input type="submit" className="submitBtn btn-info text-white" />
+                    </form>
+                </div>
+            </div>
+        </section>
     );
 };
 
