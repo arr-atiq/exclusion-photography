115a8e05becd2000be9c4a736b3de3b26a97451d
aar-atiq <aar.msdeveloper01@gmail.com>
2021-04-18 08:27:04 +0600

    divided into user and admin

--- src/Components/Dashboard/Sidebar/Sidebar.js
+++ src/Components/Dashboard/Sidebar/Sidebar.js
@@ -1,14 +1,25 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './Sidebar.css';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog, faSignOutAlt, faCalendar, faHome, faGripHorizontal, faUserPlus, faUsers } from '@fortawesome/free-solid-svg-icons';
-import { faFileAlt } from '@fortawesome/free-regular-svg-icons'
+import { faSignOutAlt, faHome, faGripHorizontal, faUserPlus, faUsers, faListUl } from '@fortawesome/free-solid-svg-icons';
+import { faAddressBook, faFileAlt, faListAlt } from '@fortawesome/free-regular-svg-icons'
 import { UserContext } from '../../../App';
 
 const Sidebar = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-    const [isDoctor, setIsDoctor] = useState(false);
+    const [isPhotographer, setIsPhotographer] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/isPhotographer', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ email: loggedInUser.email })
+        })
+            .then(res => res.json())
+            .then(data => setIsPhotographer(data));
+    }, [])
+
     return (
         <div className="sidebar d-flex flex-column justify-content-between col-md-2  py-5 px-4">
             <ul className="list-unstyled">
@@ -18,10 +29,10 @@ const Sidebar = () => {
                         <FontAwesomeIcon icon={faHome} /> <a href="/home">Home</a>
                     </Link>
                 </li>
-                {<div>
+                {isPhotographer &&<div>
                     <li>
                         <Link to="/orderList" >
-                            <FontAwesomeIcon icon={faCalendar} /> <a href="/order">Order List</a>
+                            <FontAwesomeIcon icon={faListUl} /> <a href="/order">Order List</a>
                         </Link>
                     </li>
                     <li>
@@ -42,12 +53,12 @@ const Sidebar = () => {
                 </div>}
                 <li>
                     <Link to="/bookingList"  >
-                        <FontAwesomeIcon icon={faCog} /> <a href="/bookingList">Booking List</a>
+                        <FontAwesomeIcon icon={faListAlt} /> <a href="/bookingList">Booking List</a>
                     </Link>
                 </li>
                 <li>
                     <Link to="/review"  >
-                        <FontAwesomeIcon icon={faCog} /> <a href="/review">Review</a>
+                        <FontAwesomeIcon icon={faAddressBook} /> <a href="/review">Review</a>
                     </Link>
                 </li>
             </ul>
